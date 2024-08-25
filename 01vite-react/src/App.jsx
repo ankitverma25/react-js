51b712a4ac00c2c88a01af02506bcfa06dcbe015
ankitverma25 <av9696560@gmail.com>
2024-08-25 12:57:55 +0530

    starting react

---
+++ 01vite-react/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import Homepage from './Homepage'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Homepage />
+      <div>
+       <h1>chai aur code| ankit verma</h1>
+        <p>
+          
+        </p>
+      </div>
+ 
+    </>
+  )
+}
+
+export default App
---
+++ 01vite-react/src/Homepage.jsx
@@ -0,0 +1,20 @@
+function Homepage(){
+    return(
+        <>
+        <div>
+            <ul>
+                <li>home</li>
+                <li>about</li>
+                <li>contact</li>
+                <li>product</li>
+                <li>branch</li>
+            </ul>
+        </div>
+        
+        </>
+    )
+
+
+}
+
+export default Homepage
