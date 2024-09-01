8418e1967eed8491554d1ea2a4c53dd646548dc5
ankitverma25 <av9696560@gmail.com>
2024-09-01 11:40:17 +0530

    start getting project number 5

--- 04project/src/App.jsx
+++ 04project/src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import Colorbar from './components/colorbar'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
