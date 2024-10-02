8f8ba2609df7248caadd64fa72d8a3e8920de2f1
ankitverma25 <av9696560@gmail.com>
2024-10-02 10:20:30 +0530

    make all function 0f context api

--- 09_taskmanager/src/App.jsx
+++ 09_taskmanager/src/App.jsx
@@ -1,11 +1,38 @@
+import { useState } from "react"
+import { Todoprovider } from "./component/Todo"
+
 
 
 function App() {
 
+  const [todos,setTodos]=useState([{
+    id:1,
+    title:"Buy Milk",
+    completed:false
+  }])
+
+  const addTodo=(todo)=>{
+    setTodos([...todos,{todo}]);
+  };
+
+  const deleteTodo=(id)=>{
+    setTodos(todos.filter((todo)=>todo.id!==id));
+  }
+
+  const editTodo=(id,updatedtext)=>{
+    setTodos(todos.map((todo)=>todo.id===id?{...todo,title:updatedtext}:todo));
+  }
+
+  const toggleTodo=(id)=>{
+    setTodos(todos.map((todo)=>todo.id===id?{...todo,completed:!todo.completed}:todo));
+  }
+
+
   return (
-    <>
+    <Todoprovider value={{todos,addTodo,deleteTodo,editTodo,toggleTodo}}>
+
      
-    </>
+    </Todoprovider>
   )
 }
 
--- 09_taskmanager/src/component/Todo.js
+++ 09_taskmanager/src/component/Todo.js
@@ -3,7 +3,7 @@ import { createContext } from "react";
 
 
 export const Todocontext=createContext({
-    todo:[],
+    todos:[],
     addTodo:()=>{},
     deleteTodo:()=>{},
     editTodo:()=> {},
